78425e1ca19baca1bcb780fd561dc80c8bee658d
201796838 <201796838@C0560-POSTE10>
2018-04-12 14:34:06 -0400

    debut projet

---
+++ Serveur/Controller/MembreController.php
@@ -0,0 +1,34 @@
+<?php
+	require_once("../includes/modele.inc.php");
+	$tabRes=array();
+	function enregistrerMembre(){
+		global $tabRes;
+		$nom = $_POST["nom"];
+		$prenom = $_POST["prenom"];
+		$age = $_POST["age"];
+		$courriel = $_POST["courriel"];
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		try{
+
+			$requete="INSERT INTO membre VALUES(?,?,?,?,?)";
+			$unModele=new filmsModele($requete,array($username,$prenom,$nom,$age,$courriel));
+			$stmt=$unModele->executer();
+			$tabRes['action']="enregistrer";
+			$tabRes['msg']="Film bien enregistre";
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+
+	//******************************************************
+	//Contr?leur
+	$action=$_POST['action'];
+	switch($action){
+		case "enregistrer" :
+			enregistrerMembre();
+		break;
+	}
+    echo json_encode($tabRes);
+?>
