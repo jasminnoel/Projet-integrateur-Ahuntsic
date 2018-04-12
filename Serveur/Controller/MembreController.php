d10646e4d77b644b35b391da42de728aecade200
201796838 <201796838@C0560-POSTE10>
2018-04-12 15:17:48 -0400

    membre controlleur terminé, a faire un test

--- Serveur/Controller/MembreController.php
+++ Serveur/Controller/MembreController.php
@@ -1,21 +1,45 @@
 <?php
-	require_once("../includes/modele.inc.php");
+	require_once("../Models/modele.inc.php");
 	$tabRes=array();
 	function enregistrerMembre(){
 		global $tabRes;
-		$nom = $_POST["nom"];
-		$prenom = $_POST["prenom"];
-		$age = $_POST["age"];
-		$courriel = $_POST["courriel"];
-		$username = $_POST["username"];
-		$password = $_POST["password"];
+		$usr_nom = $_POST["usr_nom"];
+		$usr_prenom = $_POST["usr_prenom"];
+		$usr_email = $_POST["usr_email"];
+		$usr_password = $_POST["usr_password"];
+    $usr_dateN = $_POST["usr_dateN"];
+    $usr_ville = $_POST["usr_ville"];
+    $usr_pays = $_POST["usr_pays"];
+    
+    
 		try{
-
-			$requete="INSERT INTO membre VALUES(?,?,?,?,?)";
-			$unModele=new filmsModele($requete,array($username,$prenom,$nom,$age,$courriel));
+    $Verification="SELECT * FROM connexion WHERE Usr_Email=?";
+			$unModele=new filmsModele($requette,array($usr_email));
+			$stmt=$unModele->executer();
+    if($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
+    
+     $tabRes['action']="enregistrer";
+			$tabRes['msg']="Membre deja existant";
+ 
+    }
+    else{
+     //ajout dans la connexion
+    $requete="INSERT INTO connexion VALUES(?,?,?)";
+			$unModele=new filmsModele($requete,array($usr_email,$usr_password,'actif'));
+			$stmt=$unModele->executer();
+    
+ 
+    
+    
+    //Ajout du membre
+			$requete="INSERT INTO utilisateurs VALUES(?,?,0,?,?,?,?)";
+			$unModele=new filmsModele($requete,array($usr_dateN,$usr_email,$usr_nom,$usr_pays,$usr_prenom,$usr_ville));
 			$stmt=$unModele->executer();
 			$tabRes['action']="enregistrer";
-			$tabRes['msg']="Film bien enregistre";
+			$tabRes['msg']="Membre bien enregistre";
+    }
+    
+  
 		}catch(Exception $e){
 		}finally{
 			unset($unModele);
