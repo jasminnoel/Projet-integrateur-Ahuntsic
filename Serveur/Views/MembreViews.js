e9c7bf954c843c405dffaff74c1abd8fd4d76c1c
mysco123 <37550119+mysco123@users.noreply.github.com>
2018-04-20 15:53:07 -0400

    update vue pour modal

--- Serveur/Views/MembreViews.js
+++ Serveur/Views/MembreViews.js
@@ -14,7 +14,7 @@ function vueUser(reponse) {
     rep += "                                  <i class=\"material-icons grey-text lighten-3\">perm_contact_calendar</i>\n";
     rep += "                              </div>\n";
     rep += "                              <div class=\"col s12 m8\">\n";
-    rep += "                                  <span class=\"\">Vous avez 4 nouvelles invitations. Confirmez votre présence dès maintenant!</span>\n";
+    rep += "                                  <span class=\"\">Vous avez 4 nouvelles invitations. Confirmez votre prÃ©sence dÃ¨s maintenant!</span>\n";
     rep += "                              </div>\n";
     rep += "                              <div class=\"col s12 m3\">\n";
     rep += "                                  <div class=\"right\">\n";
@@ -27,28 +27,28 @@ function vueUser(reponse) {
     rep += "                  </div>\n";
     rep += "                  <!-- Carte - Messagerie -->\n";
     rep += "                  <div class=\"row\">\n";
-    rep += "                      <h5>Événements à venir</h5>\n";
+    rep += "                      <h5>Ã‰vÃ©nements Ã  venir</h5>\n";
     rep += "                      <div class=\"collection\">\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
     rep += "                          </a>\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
     rep += "                          </a>\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
     rep += "                          </a>\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
@@ -56,22 +56,22 @@ function vueUser(reponse) {
     rep += "                      </div>\n";
     rep += "                  </div>\n";
     rep += "                  <div class=\"row\">\n";
-    rep += "                      <h5>Événements archivés</h5>\n";
+    rep += "                      <h5>Ã‰vÃ©nements archivÃ©s</h5>\n";
     rep += "                      <div class=\"collection\">\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
     rep += "                          </a>\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
     rep += "                          </a>\n";
     rep += "                          <a class=\"collection-item\" href=\"#\">\n";
-    rep += "                              <span class=\"title\">Camping en Gaspésie</span>\n";
+    rep += "                              <span class=\"title\">Camping en GaspÃ©sie</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">group</i>4</span>\n";
     rep += "                              <span class=\"secondary-content valign-wrapper\"><i class=\"material-icons\">email</i>4</span>\n";
     rep += "                              <br /><span>02/05/2018</span>\n";
@@ -79,12 +79,58 @@ function vueUser(reponse) {
     rep += "                      </div>\n";
     rep += "                  </div>\n";
     rep += "              </div><!-- FIN colonne -->\n";
+
+      rep += " <div id=\"modal1\" class=\"\">";
+    rep += "   <div class=\"\">";
+    rep += "       <h4>Creation d'un evenement</h4>";
+    rep += "       <div class=\"row\">";
+    rep += "          <form name=\"form_event\" id=\"form_event\" class=\"col s12\">";
+    rep += "               <div class=\"row\">";
+    rep += "                    <!-- Input Description -->";
+    rep += "                <div class=\"input - field col s6\">";
+    rep += "                        <input id=\"Event_Nom\" name=\"Event_Nom\" type=\"text\" class=\"validate\">";
+    rep += "                            <label for=\"Event_Nom\">Nom</label>";
+    rep += "                </div>";
+    rep += "                       <!-- Input lieu -->";
+    rep += "                <div class=\"input - field col s6\">";
+    rep += "                           <input id=\"Event_Lieu\" name=\"Event_Lieu\" type=\"text\" class=\"validate\">";
+    rep += "                               <label for=\"Event_Lieu\">Lieu</label>";
+    rep += "                </div>";
+    rep += "                </div>";
+    rep += "                <!-- Input nom -->";
+    rep += "    <div class=\"row\">";
+    rep += "                    <div class=\"input - field col s12\">";
+    rep += "                        <input id=\"Event_Desc\" name=\"Event_Desc\" type=\"text\" class=\"validate\">";
+    rep += "                            <label for=\"Event_Desc\">Description</label>";
+    rep += "        </div>";
+    rep += "                    </div>";
+    rep += "                    <!-- Input Date de l'evenement -->";
+    rep += "    <div class=\"row\">";
+    rep += "                        <div class=\"input - field col s12\">";
+    rep += "                            <input id=\"Event_Date_Debut\" name=\"Event_Date_Debut\" type=\"date\" class=\"validate\">";
+    rep += "                                <label for=\"Event_Date_Debut\">Date de l'evenement</label>";
+    rep += "        </div>";
+    rep += "                        </div>";
+    rep += "                        <!-- Input Heure de l'event -->";
+    rep += "    <div class=\"row\">";
+    rep += "                            <div class=\"input - field col s12\">";
+    rep += "                                <input id=\"Event_Time\" name=\"Event_Time\" type=\"text\" class=\"validate\">";
+    rep += "                                    <label for=\"Event_Time\">Heure de debut</label>";
+    rep += "        </div>";
+    rep += "                            </div>";
+    rep += "</form>";
+    rep += "                    </div>";
+    rep += "                    <div class=\"\">";
+    rep += "                        <button class=\"btn waves - effect waves - light\" type=\"button\" onclick=\"ValidEvent()\" name=\"action\">CrÃ©er</a>";
+    rep += "                    </div>";
+    rep += "                </div>";
+
     rep += "\n";
     rep += "              <div id=\"col-droite\" class=\"col s12 m4\">\n";
-    rep += "                  <!-- Carte - Créer evenement -->\n";
+    rep += "                  <!-- Carte - CrÃ©er evenement -->\n";
     rep += "                  <div class=\"card teal right\">\n";
     rep += "                      <div class=\"card-content\">\n";
-    rep += "                          <p class=\"flow-text white-text center-align\">Créez votre propre événement!</p>\n";
+    rep += "                          <p class=\"flow-text white-text center-align\">CrÃ©ez votre propre Ã©vÃ©nement!</p>\n";
     rep += "                      </div>\n";
     rep += "                      <div class=\"card-action\">\n";
     rep += "                          <a class=\"btn-floating halfway-fab waves-effect waves-light red btn-large\"><i class=\"material-icons\">add</i></a>\n";
@@ -94,8 +140,9 @@ function vueUser(reponse) {
     rep += "              </div>\n";
     rep += "          </div><!-- FIN row -->\n";
 
+
     var repMenu = "";
-    repMenu += "<li><a class=\"waves-effect waves-light btn\"><i class=\"material-icons right\">add</i>Créer événement</a></li>\n";
+    repMenu += "<li><a class=\"waves-effect waves-light btn modal-trigger\" href=\"#modal1\"><i class=\"material-icons right\">add</i>CrÃ©er Ã©vÃ©nement</a></li>\n";
     repMenu += "                  <li><a href=\"#\"><i class=\"material-icons teal-text\">event</i></a></li>\n";
     repMenu += "                  <li><a href=\"#\"><i class=\"material-icons teal-text\">person_pin</i></a></li>\n";
 
@@ -118,3 +165,4 @@ var membreVue=function(reponse){
             break;
 	}
 }
+
