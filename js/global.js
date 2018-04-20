9875a986ba2f39c568c4cb13ccc4eb3945441e6e
mysco123 <37550119+mysco123@users.noreply.github.com>
2018-04-20 15:55:00 -0400

    jquery pour modal mais work pas

---
+++ js/global.js
@@ -0,0 +1,14 @@
+(function ($) {
+    $(function () {
+
+        //initialize all modals           
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
