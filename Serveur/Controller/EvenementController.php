b665e5d71a10539f2f79cd07e5ee4d0064f7adb5
mysco123 <37550119+mysco123@users.noreply.github.com>
2018-05-01 21:41:30 -0400

    Update EvenementController.php

--- Serveur/Controller/EvenementController.php
+++ Serveur/Controller/EvenementController.php
@@ -6,7 +6,7 @@ function affEvenement() {
 global $tabRes;
 session_start();
 $Event_ID = $_POST["Event_ID"];
-
+$_SESSION["Event_ID"] = $Event_ID;
 $Usr_ID = $_SESSION["Usr_ID"];
 
 try{
@@ -14,7 +14,7 @@ try{
 			$unModele=new filmsModele($reqEvent,array($Event_ID));
 			$stmt=$unModele->executer();
 			$ligne=$stmt->fetch(PDO::FETCH_OBJ);
-		
+
     $tabRes['action']="affEvenement";
     $tabRes['objEve']=$ligne;
 
@@ -23,20 +23,20 @@ try{
 			$stmt=$unModele->executer();
 	$tabRes['listeContri']=array();
 			while($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
-      
-	$tabRes['listeContri'][] = $ligne;		
-			
+
+	$tabRes['listeContri'][] = $ligne;
+
       }
 $tabRes['listeinvit'] = array();
 $reqEvent="SELECT invitations.Invit_Statut,utilisateurs.Usr_Photo,utilisateurs.Usr_Prenom FROM invitations inner JOIN utilisateurs ON invitations.Usr_ID=utilisateurs.Usr_ID WHERE invitations.Event_ID=?";
 			$unModele=new filmsModele($reqEvent,array($Event_ID));
 			$stmt=$unModele->executer();
 			while($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
-      
-	$tabRes['listeinvit'][] = $ligne;		
-			
+
+	$tabRes['listeinvit'][] = $ligne;
+
       }
-	
+
 	//affichage des messages chat
 					$reqEvent="SELECT * FROM messages inner Join utilisateurs ON messages.Usr_ID=utilisateurs.Usr_ID WHERE Event_ID=? ORDER BY Mess_Date";
 						$unModele=new filmsModele($reqEvent,array($Event_ID));
@@ -47,7 +47,7 @@ $reqEvent="SELECT invitations.Invit_Statut,utilisateurs.Usr_Photo,utilisateurs.U
 				$tabRes['ListMessages'][] = $ligne;
 
 			      }
-      
+
 		}catch(Exception $e){
 		}finally{
 			unset($unModele);
@@ -63,7 +63,7 @@ session_start();
 $Usr_ID = $_SESSION["Usr_ID"];
 
 try{
-			$reqEvent="	SELECT * 
+			$reqEvent="	SELECT *
 						FROM evenements
 						INNER JOIN Invitations ON evenements.Event_ID = Invitations.Event_ID
 						WHERE Status=i AND Usr_ID=?"; // i: invité, p: participant
@@ -72,7 +72,7 @@ try{
 			$ligne=$stmt->fetch(PDO::FETCH_OBJ);
 
 			$nbrIvitation++;
-		
+
     $tabRes['action']="affInvitation";
     $tabRes['objInvit']=$ligne;
     $tabRes['nbrIvitation']=$nbrIvitation;
@@ -106,6 +106,20 @@ function ajouterContri(){
 					}
 }
 
+function Add_Messages_Controller(){
+	global $tabRes;
+	session_start();
+	$Usr_ID = $_SESSION["Usr_ID"];
+	$Event_ID = $_SESSION["Event_ID"];
+	$Mess_Contenu = $_POST["Mess_Contenu"];
+	$Mess_Date = date("Y-m-d");
+
+	$Query="INSERT INTO messages VALUES(?,0,?,?,?)";
+	$unModele=new filmsModele($Query,array($Usr_ID, $Event_ID,$Mess_Contenu, $Mess_Date));
+	$stmt=$unModele->executer();
+
+}
+
 
 		//******************************************************
 	//Controleur
@@ -116,10 +130,13 @@ function ajouterContri(){
 		break;
 		case "affInvitation" :
 			affInvitation();
-		break;	
+		break;
 		case "ajouterContri" :
 			ajouterContri();
-		break;	
+		break;
+		case "AjoutMessage" :
+			Add_Messages_Controller();
+		break;
 	}
     echo json_encode($tabRes);
 
