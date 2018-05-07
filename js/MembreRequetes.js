73cefb14f7182c4f4a1ea24a01bcfaa136dc29a1
mysco123 <37550119+mysco123@users.noreply.github.com>
2018-05-07 15:46:19 -0400

    Update MembreRequetes.js

--- js/MembreRequetes.js
+++ js/MembreRequetes.js
@@ -1,3 +1,34 @@
+window.onload = function doSomething() {
+    var myCookie = getCookie("PHPSESSID");
+
+    if (myCookie == null) {
+        // do cookie doesn't exist stuff;
+    }
+    else {
+        RetourMembre();
+    }
+}
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
 //requetes membre
 function inscrireMembre(){
     var formMembre = new FormData(document.getElementById('form_membre'));
